442d25c916634f514bb8d425effac54469bf30f4
marco stefani <marcolikis@gmail.com>
2021-01-25 14:24:04 -0300

    space

--- riscV32I/source/readEccInformation.c
+++ riscV32I/source/readEccInformation.c
@@ -1,15 +1,15 @@
 #include "../library/absimth.hpp"
 
 int main(void) {
-	 int *locationEcc = (int*)0x4;
-	 int *zeroAddress = (int*)0x0;
+	int *locationEcc = (int *)0x4;
+	int *zeroAddress = (int *)0x0;
 
 	int globalAppInitialAddress = read_initial_address();
 	int ecc_address_content_last_adress_failed = *(locationEcc - globalAppInitialAddress);
 	print_int(ecc_address_content_last_adress_failed);
 
-	 int read_failed_address = *(zeroAddress - globalAppInitialAddress + ecc_address_content_last_adress_failed);
-	 print_int(read_failed_address);
-	
+	int read_failed_address = *(zeroAddress - globalAppInitialAddress + ecc_address_content_last_adress_failed);
+	print_int(read_failed_address);
+
 	return 0;
 }
